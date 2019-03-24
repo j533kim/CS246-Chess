682892149b693c82e2de62238799cdeac54bf957
Jiwook Kim <kimjiwook0129@gmail.com>
2019-03-24 16:54:10 -0400

    added helper functions for the main function, and board module

---
+++ board.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+#include "board.h"
+
+using namespace std;
+
---
+++ board.h
@@ -0,0 +1,33 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "cell.h"
+#include "textdisplay.h"
+#include "graphicsdisplay.h"
+#include "color.h"
+
+class TextDisplay;
+template <typename InfoType, typename StateType> class Observer;
+class Board {
+  std::vector<std::vector<Cell>> theBoard;
+  TextDisplay *td = nullptr;
+  Observer<Info, State> *ob = nullptr;
+  bool checkmate = false;
+  boo stalemate = false;
+public:
+  ~Board(); // destructor
+  void setObserver(Observer<Info, State> *ob);
+  void init(); // sets up 8x8 board and responsible for observer pattern
+               // clears the old board, if necessary
+  void move(std::string pos_initial, std::string pos_final);
+  void remove(std::string pos);
+  Color winner(); // white wins -> White, black wins -> Black, draw -> NoColor
+  bool gameEnd(); // true if gameover or false otherwise 
+  placePiece(std::string piece, std::string pos);
+  // placePiece() calls move and remove functions
+};
+
+#endif
