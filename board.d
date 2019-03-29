05ba1de6e23071df821eafc5d7567e0d2805c935
Nipun <nipun00123@gmail.com>
2019-03-29 17:03:36 -0400

    small c

--- board.d
+++ board.d
@@ -1,3 +1,3 @@
-board.o: board.cc board.h cell.h piece.h color.h observer.h subject.h \
-  state.h textdisplay.h main_helper.h king.h queen.h rook.h bishop.h \
+board.o: board.cc board.h textdisplay.h color.h cell.h piece.h observer.h \
+  subject.h state.h main_helper.h king.h queen.h rook.h bishop.h \
   knight.h pawn.h nopiece.h
--- main.d
+++ main.d
@@ -1,3 +1,3 @@
-main.o: main.cc board.h cell.h piece.h color.h observer.h subject.h \
-  state.h textdisplay.h main_helper.h computer.h king.h queen.h rook.h \
+main.o: main.cc board.h textdisplay.h color.h cell.h piece.h observer.h \
+  subject.h state.h main_helper.h computer.h king.h queen.h rook.h \
   bishop.h knight.h pawn.h
