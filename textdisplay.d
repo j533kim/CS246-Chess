24ca19940002eb26625a20f94276914fec55a7c9
Jiwook Kim <kimjiwook0129@gmail.com>
2019-03-30 02:45:28 -0400

    shared_ptrs used

--- textdisplay.d
+++
@@ -1,3 +0,0 @@
-textdisplay.o: textdisplay.cc textdisplay.h color.h cell.h piece.h \
-  observer.h subject.h state.h king.h queen.h rook.h bishop.h knight.h \
-  pawn.h board.h main_helper.h nopiece.h
