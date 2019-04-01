4110f751e73d0ae80f6a7f33b47178d153afa18f
Prabhapaar Singh Batra <psbatra@uwaterloo.ca>
2019-04-01 18:11:41 -0400

    computer changes

--- computer.cc
+++ computer.cc
@@ -34,7 +34,7 @@ void computer_1(Board &b, Color color) {
 	int totalpieces = 0;                     // total no. of pieces in MyPieces array
 	bool w_turn = 0;                         // equals 1 when it's whites's turn
 	if (Color::color == Color::White) {
-    	w_turn = true;
+    	w_turn = 1;
 		for (int i = 0; i < 8; ++i) {
 			for (int j = 0; j < 8; ++j) {
 				if (b.at(i).at(j).getPiece()->getColor() == Color::White) {
@@ -67,8 +67,9 @@ void computer_1(Board &b, Color color) {
     		int row = rand() % 8;
     		int col = rand() % 8;
     		if (b.canmove(MyPieces.at(piece_index).getName(), Row_Col.at(piece_index).at(0), Row_Col.at(piece_index).at(1), row, col)) {
-    			b.move(initial, final, w_turn); // confirm the format for initial and final positions
-    			//add the notify function 
+    			string initial = ourpos_to_user(Row_Col.at(piece_index).at(0), Row_Col.at(piece_index).at(1));
+    			string final = ourpos_to_user(row, col)
+    			b.move(initial, final, w_turn); 
     			return;
     		} 
     	}
@@ -79,8 +80,9 @@ void computer_1(Board &b, Color color) {
     	for (int r = 0; r < 8; ++r) {
     		for (int c = 0; c < 8; ++c) {
     			if (b.canmove(MyPieces.at(m).getName(), Row_Col.at(m).at(0), Row_Col.at(m).at(1), r, c)) {
-    				b.move(initial, final, w_turn); // confirm the format for initial and final positions
-    				//add the notify function 
+    				string initial = ourpos_to_user(Row_Col.at(m).at(0), Row_Col.at(m).at(1));
+    				string final = ourpos_to_user(r, c)
+    				b.move(initial, final, w_turn); 
     				return;
     			} 
     		}
@@ -97,7 +99,7 @@ void computer_2(Board &b, Color color) {
 	int op_totalpieces = 0;                     // total no. of pieces in OpPieces array
 	bool w_turn = 0;                         // equals 1 when it's whites's turn
 	if (Color::color == Color::White) {
-    	w_turn = true;
+    	w_turn = 1;
 		for (int i = 0; i < 8; ++i) {
 			for (int j = 0; j < 8; ++j) {
 				if (b.at(i).at(j).getPiece()->getColor() == Color::White) {
@@ -154,7 +156,9 @@ void computer_2(Board &b, Color color) {
 		}
 		for (int m = 0; m < my_totalpieces; ++m) {
 			if (b.canmove(MyPieces.at(m).getName(), Row_Col_MyPieces.at(m).at(0), Row_Col_MyPieces.at(m).at(1), Row_Col_OpPieces.at(k).at(0), Row_Col_OpPieces.at(k).at(1))) {
-    			b.move(initial, final, w_turn); // confirm the format for initial and final positions
+				string initial = ourpos_to_user(Row_Col_MyPieces.at(m).at(0), Row_Col_MyPieces.at(m).at(1));
+    			string final = ourpos_to_user(Row_Col_OpPieces.at(k).at(0), Row_Col_OpPieces.at(k).at(1))
+    			b.move(initial, final, w_turn); 
     			return;
     		} 
 		}
